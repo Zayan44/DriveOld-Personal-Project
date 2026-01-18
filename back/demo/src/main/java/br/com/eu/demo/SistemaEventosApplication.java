ea44f11ff000b880da5cae683a4a914c7836a45a
Murilo Duarte <murilo.almeida.dev@outlook.com>
2026-01-18 13:06:17 -0300

    commit inicial

---
+++ back/demo/src/main/java/br/com/eu/demo/SistemaEventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.eu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventosApplication.class, args);
+	}
+
+}
---
+++ back/demo/src/test/java/br/com/eu/demo/SistemaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

87bc506aeec291720af63fb3a4baa143789661ed
Murilo Duarte <murilo.almeida.dev@outlook.com>
2026-01-18 13:50:27 -0300

    Initial Commit

--- back/siseventos/src/main/java/br/com/personalproject/siseventos/SiseventosApplication.java
+++ back/siseventos/src/main/java/br/com/personalproject/siseventos/SiseventosApplication.java
@@ -1,13 +1,13 @@
-package br.com.eu.demo;
+package br.com.personalproject.siseventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaEventosApplication {
+public class SiseventosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaEventosApplication.class, args);
+		SpringApplication.run(SiseventosApplication.class, args);
 	}
 
 }
--- back/siseventos/src/test/java/br/com/personalproject/siseventos/SiseventosApplicationTests.java
+++ back/siseventos/src/test/java/br/com/personalproject/siseventos/SiseventosApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.eu.demo;
+package br.com.personalproject.siseventos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SistemaEventosApplicationTests {
+class SiseventosApplicationTests {
 
 	@Test
 	void contextLoads() {
